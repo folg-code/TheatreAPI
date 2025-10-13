ca05cdf94fa75d59cf7689aa97f78cc5c3ae7a50
filip <filip1folga@gmail.com>
2025-10-13 10:30:45 +0200

    create basic serializers

--- theatre_service/models.py
+++ theatre_service/models.py
@@ -26,8 +26,8 @@ class Genre(models.Model):
 class Play(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    actors = models.ManyToManyField(Actor)
-    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor, blank=True)
+    genres = models.ManyToManyField(Genre, blank=True)
 
     def __str__(self):
         return self.title
---
+++ theatre_service/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from theatre_service.models import Actor, Genre, TheatreHall, Performance, Play
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name")
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Play
+        fields = ("id", "title", "description", "actor", "genre")
+
+
+class TheatreHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TheatreHall
+        fields = ("id", "name", "rows", "seat_in_row", "capacity")
+
+
+class PerformanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Performance
+        fileds = ("id", "play", "theatre_hall", "show_time")
