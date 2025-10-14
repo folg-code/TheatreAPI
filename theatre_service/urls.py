0417ddc46663cc7b6d49ffeb398403fa50931d41
filip <filip1folga@gmail.com>
2025-10-14 11:04:55 +0200

    create urls for each endpoint

---
+++ theatre_service/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from theatre_service.views import PerformanceViewSet, TheatreHallViewSet, PlayViewSet, ActorViewSet, GenreViewSet
+
+app_name = 'theatre_service'
+
+router = DefaultRouter()
+
+router.register('performers', PerformanceViewSet, basename='performers')
+router.register('halls', TheatreHallViewSet, basename='halls')
+router.register('plays', PlayViewSet, basename='plays')
+router.register('actors', ActorViewSet, basename='actors')
+router.register('genres', GenreViewSet, basename='genres')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
