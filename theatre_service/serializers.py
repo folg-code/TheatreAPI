07e604e5be7b11d168080c745348696b3afa4eab
filip <filip1folga@gmail.com>
2025-10-14 12:57:57 +0200

    create Order, Ticket serializers

--- theatre_service/serializers.py
+++ theatre_service/serializers.py
@@ -1,7 +1,9 @@
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField, PrimaryKeyRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-from theatre_service.models import Actor, Genre, TheatreHall, Performance, Play
+from theatre_service.models import Actor, Genre, TheatreHall, Performance, Play, Ticket, Order
 
 
 class ActorSerializer(serializers.ModelSerializer):
@@ -87,3 +89,83 @@ class PerformanceListSerializer(PerformanceSerializer):
     class Meta:
         model = Performance
         fields = ("id", "movie", "cinema_hall_capacity", "movie_title", "cinema_hall_name")
+
+
+class TicketPerformanceSerializer(serializers.ModelSerializer):
+    play_title = serializers.CharField(source="play.title", read_only=True)
+    show_time = serializers.CharField(read_only=True)
+    theatre_hall_name = serializers.CharField(
+        source="theatre_hall.name",
+        read_only=True,
+    )
+    cinema_hall_capacity = serializers.IntegerField(
+        source="cinema_hall.capacity",
+        read_only=True,
+    )
+    class Meta:
+        model = Performance
+        fileds = ("id", "play_title", "show_time",
+                  "theatre_hall_name", "cinema_hall_capacity")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    performance = TicketPerformanceSerializer(many=True, read_only=True)
+    performance_id = serializers.PrimaryKeyRelatedField(
+        queryset=Performance.objects.all(),
+        source="performance",
+        write_only=True,
+    )
+    class Meta:
+        model = Ticket
+        fields = ("id", "row", "seat_in_row", "performance", "performance_id")
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Ticket.objects.all(),
+                fields=["row", "seat_in_row", "performance"],
+                message="This ticket already exists."
+            ),
+        ]
+
+
+class TicketRowSeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat_in_row")
+
+    def validate(self, data):
+
+        performance = data.get('performance')
+        row = data.get('row')
+        seat_in_row = data.get('seat_in_row')
+
+        if performance:
+            hall = performance.theatre_hall
+
+            if not (1 <= row <= hall.rows):
+                raise serializers.ValidationError(
+                    {"row": f"Row must be between 1 and {hall.rows}."}
+                )
+
+            if not (1 <= seat_in_row <= hall.seat_in_row):
+                raise serializers.ValidationError(
+                    {"seat": f"Seat must be between 1 and {hall.seat_in_row}."}
+                )
+
+        return data
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ("id", "created_at", "tickets")
+
+    def create(self, validated_data):
+        tickets_data = validated_data.pop("tickets")
+        with transaction.atomic():
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+        return order
